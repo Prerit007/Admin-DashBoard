cd9355d9782a69b6b5036a209d10751bd7661895
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-15 22:03:04 +0530

    Initial Commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Button from "./Button";
+import Chart from "./Chart";
+import Chat from "./Chat";
+import ChartsHeader from "./ChartsHeader";
+import Footer from "./Footer";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import ThemeSettings from "./ThemeSettings";
+import UserProfile from "./UserProfile";
+import Notification from "./Notification";
+import LineChart from "./Charts/LineChart";
+import Pie from "./Charts/Pie";
+import SparkLine from "./Charts/SparkLine";
+import Stacked from "./Charts/Stacked";
+
+export default {
+  Button,
+  Chart,
+  Chat,
+  ChartsHeader,
+  Footer,
+  Sidebar,
+  ThemeSettings,
+  Header,
+  Navbar,
+  UserProfile,
+  Notification,
+  LineChart,
+  SparkLine,
+  Pie,
+  Stacked,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import Calendar from "./Calendar";
+import ColorPicker from "./ColorPiimport ColorPicker";
+import Customers from "./Customers";
+import Ecommerce from "./Ecommerce";
+import Editor from "./Editor";
+import Employees from "./Employees";
+import Kanban from "./Kanban";
+import Orders from "./Orders";
+import Area from "./Charts/Area";
+import Bar from "./Charts/Bar";
+import ColorMapping from "./Charts/ColorMapping";
+import Financial from "./Charts/Financial";
+import Line from "./Charts/Line";
+import Pie from "./Charts/Pie";
+import Pyramid from "./Charts/Area";
+import Stacked from "./Charts/Stacked";
+
+export default {
+  Calendar,
+  ColorPicker,
+  Customers,
+  Ecommerce,
+  Editor,
+  Employees,
+  Kanban,
+  Orders,
+  Area,
+  Bar,
+  ColorMapping,
+  Financial,
+  Line,
+  Pie,
+  Pyramid,
+  Stacked,
+};
