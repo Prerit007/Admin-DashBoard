f0dca586925f860defa6dd01b8196df36521c414
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-21 02:52:01 +0530

    commit

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -9,7 +9,13 @@ const change = (args) => {
 };
 
 const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Color Picker" />
+    </div>
+  );
 };
 
 export default ColorPicker;
+
+//2:54:50
