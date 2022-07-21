a2dac91aeecabb179f79abc3570d99e75e95dcec
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-21 17:09:01 +0530

    commit

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -18,7 +18,28 @@ import {
 import { useStateContext } from "../../contexts/ContextProvider";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      legendSettings={{ background: "white" }}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
