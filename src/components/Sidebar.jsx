186e3c66115083bff2ac9be7ad6a0e92caa1b812
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-21 17:35:10 +0530

    changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -32,7 +32,7 @@ const Sidebar = () => {
               onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Shoppy</span>
+              <SiShopware /> <span>PLecto</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
